b39cbb52dfdaa63a05a6f53326f9c7342e466dc6
Micg25 <gglmhl02l25c351x@studium.unict.it>
2026-02-08 00:04:00 +0100

    adjust contrast filter added

--- filtri.py
+++ filtri.py
@@ -6,3 +6,9 @@ def mask(immagine_input):
     cv2.rectangle(matrice_maschera, (500, 340), (800, 720), 255, -1)
     immagine_mascherata = cv2.bitwise_and(immagine_input, immagine_input, mask=matrice_maschera)
     return immagine_mascherata
+
+#Modifica luminosità e contrasto
+def adjust_contrast(immagine_input, alpha=1.0, beta=0):
+    immagine_regolata = cv2.convertScaleAbs(immagine_input, alpha=alpha, beta=beta)
+    return immagine_regolata
+
